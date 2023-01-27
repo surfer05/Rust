06a41d46e455f081dc32fca439499530070902b7
surfer05 <rahul05alok@gmail.com>
2023-01-28 03:38:04 +0530

    Chapter 9 completed

--- error_handling/src/bin/result.rs
+++ error_handling/src/bin/result.rs
@@ -34,8 +34,82 @@ fn main() {
     //           Result is the Err variant, unwrap will call the panic!
 
     // unwrap in action
-    let greeting
+    let greeting_file = File::open("hello.txt").unwrap();
+
+    // expect : the `expect` method lets us also choose the panic! error message. Using expect instead of unwrap and providing good error messages can convey your intent and make tracking down the source of a panic easier. 
+
+    // expect in action
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be incuded in this project.");
+
+        
+    }
+    
+// PROPAGATING ERRORS
+
+fn read_username_from_file()-> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result{
+        Ok(file)=>file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+    match username_file.read_to_string(&mut username){
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+
+
+    // A Shortcut for Propagating Errors: the ? Operator
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let mut username_file = File::open("hello.txt")?;
+        let mut username = String::new();
+        // Value of result is Ok : value inside Ok is returned
+        // If the value is Err : Err will be returned from whole function
+        username_file.read_to_string(&mut username)?;
+        Ok(username)
+    }
+    // We’re only allowed to use the ? operator in a function that returns Result, Option, or another type that implements FromResidual.
+
+    // USE fs::read_to_string instead of opening and the reading the file
+    fn read_username_from_file()->{
+        Result<String,io::Error>{
+            // fs::read_to_string function that opens the file, creates a new String, reads the contents of the file, puts the contents into that String, and returns it.
+            fs::read_to_string("hello.txt")
+        }
+    }
+
+    // USING the ? operator on an Option<T> value
+
+    // This function returns Option<char> because it’s possible that there is a character there, but it’s also possible that there isn’t. 
+    // This code takes the text string slice argument and calls the lines method on it, which returns an iterator over the lines in the string.
+    // Because this function wants to examine the first line, it calls next on the iterator to get the first value from the iterator.
+    // If text is the empty string, this call to next will return None, in which case we use ? to stop and return None from last_char_of_first_line.
+    // If text is not the empty string, next will return a Some value containing a string slice of the first line in text.
+    // The ? extracts the string slice, and we can call chars on that string slice to get an iterator of its characters.
+    // We’re interested in the last character in this first line, so we call last to return the last item in the iterator
+    fn last_char_of_first_line(text: &str)->Option<char> {
+        text.lines().next()?.chars().last()
+    }
+
+
+    // RETURNING ERROR THROUGH MAIN FUNCTION
+    // For now Box<dyn Error> means "any kind of error"
+    // Even though the body of this main function will only ever return errors of type std::io::Error, by specifying Box<dyn Error>, this signature will continue to be correct even if more code that returns other errors is added to the body of main.
+
+
+    fn main()->Result<(),Box<dyn Error>> {
+        let greeting_file = File::open("hello.txt")?;
+
+        Ok(())
+    }
+
+
 }
+
+
 
 
 // RESULT ENUM HAS TWO VARIANTS
